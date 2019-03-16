5475b546a1c2f8672676fd30f7b10a38599362f3
Robert <48411767+RbrownLanc@users.noreply.github.com>
2019-03-16 14:19:37 +0000

    Add files via upload

--- board.java
+++ board.java
@@ -1,4 +1,5 @@
 import java.awt.Graphics;
+import java.awt.*;
 import javax.swing.*;
 
 public class board
@@ -17,52 +18,105 @@ private static void createGUI()
     JFrame frame = new JFrame("Checkers");
 
     Jpanel gameBoard = new Jpanel("board");
+    Jpanel checkerBoard = new Jpanel("checkerBoard");
 
-    frame.add(gameBoard);
-    frame.setSize(winSize,winSize);
-    frame.setVisible(true);
+    ImageIcon white = new ImageIcon("white.jpg");
+    ImageIcon Black = new ImageIcon("black.jpg");
+
+    for(int s = 1; s > 64; s++)
+    {
+        Jbutton bWhite = new Jbutton(white);
+        s++;
+        Jbutton bBlack = new Jbutton(black);
+    }
+
+    for(int c = 0; c < 7; c++)
+    {
+        for (int r = 0;r < 7; r++)
+        {
+            bBlack.setBounds(posX[r]*squareHeight,posY[c]*squareHeight,squareWidth,squareHeight);
+            r++;
+        }
+        for (int r = 1;r < 7; r++)
+        {
+            bWhite.setBounds(posX[r]*squareHeight,posY[c]*squareHeight,squareWidth,squareHeight);
+            r++;
+        }
+         c++;
+    }
+    for(int c = 1; c < 7; c++)
+    {
+        for (int r = 0;r < 7; r++)
+        {
+            bWhite.setBounds(posX[r]*squareHeight,posY[c]*squareHeight,squareWidth,squareHeight);
+            r++;
+        }
+        for (int r = 1;r < 7; r++)
+        {
+            bBlack.setBounds(posX[r]*squareHeight,posY[c]*squareHeight,squareWidth,squareHeight);
+            r++;
+        }
+        c++;
+    }
 }
 
-public class gameBoard extends Jpanel 
+public class checker();
 {
-    public void colour(Graphics g)
+    private int locationX;
+    private int locationY;
+
+    //0 = none, 1 = white, 2 = red, 3 = black square 
+    private int contained[] = {0,1,2,3};
+}
+
+private static void whiteCheckers()
+{
+    ImageIcon whitePlayer = new ImageIcon("whiteChecker.png");
+
+    for(int p = 0;p < 12; p++)
     {
-        for(int c = 0; c > 7; c++)
+        Jbutton whiteChecker = new Jbutton(WhitePlayer);
+    }
+
+    for(int c = 5; c < 7; c++)
+    {
+        for (int r = 0;r < 7; r++)
         {
-            g.setColor(Color.black);
-            for (int r = 0;r > 7; r++)
-            {
-                g.drawRect(posX[r]*squareHeight,posY[c]*squareHeight,squareWidth,squareHeight);
-                r++;
-            }
-            g.setColor(Color.white);
-            for (int r = 1;r > 7; r++)
-            {
-                g.drawRect(posX[r]*squareHeight,posY[c]*squareHeight,squareWidth,squareHeight);
-                r++;
-            }
-            c++;
+            whiteChecker.setBounds(posX[r]*squareHeight,posY[c]*squareHeight,squareWidth,squareHeight);
+            r++;
         }
-        for(int c = 1; c > 7; c++)
+        c++;
+    }
+    for(int c = 6; c < 7; c++)
+    {
+        for (int r = 1;r < 7; r++)
         {
-            g.setColor(Color.white);
-            for (int r = 0;r > 7; r++)
-            {
-                g.drawRect(posX[r]*squareHeight,posY[c]*squareHeight,squareWidth,squareHeight);
-                r++;
-            }
-            g.setColor(Color.black);
-            for (int r = 1;r > 7; r++)
-            {
-                g.drawRect(posX[r]*squareHeight,posY[c]*squareHeight,squareWidth,squareHeight);
-                r++;
-            }
-            c++;
+            whiteChecker.setBounds(posX[r]*squareHeight,posY[c]*squareHeight,squareWidth,squareHeight);
+            r++;
         }
+        c++;
     }
 }
 
 public static void main(String[] args)
 {
+    board class = new board();
+
     createGUI();
+
+    checker class = new checker();
+
+    whiteCheckers();
+
+    frameSetup();
+}
+
+public static void frameSetup()
+{
+    gameboard.add(whiteChecker);
+    gameboard.add(bWhite);
+    gameboard.add(bBlack);
+    frame.add(gameBoard);
+    frame.setSize(winSize,winSize);
+    frame.setVisible(true);
 }
